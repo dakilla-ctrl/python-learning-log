3a128f65c00d748e6ec2362cf170b78f2ca15fea
dakilla-ctrl <dakillalord402@gmail.com>
2025-08-07 11:31:48 +0100

    practiced input, print, type casting etc.

---
+++ day01/python_basics_practice.py
@@ -0,0 +1,24 @@
+""" 
+The purpose of this this is to practice
+printing, getting input, using different data types.
+
+What this Program does - 
+- print expressions with print()
+- takes input with input()
+- concantenates strings with '+'
+- Use int() for string conversion to integer
+- Use str() for integer conversion to string 
+
+"""
+
+
+
+
+# This program places a burden! ⚠️
+
+print('Who goes there?')
+name = input()
+print('For summoning me ' + name + ' you are now burdened to lockin and get a job.')
+print('How many months do you need?')
+months = input()
+print('No, you get ' + str(int(months)-1) + ' starting today')
